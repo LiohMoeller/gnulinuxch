e3d926ff20b6372ea21b1ad2e97c6b227080e081
Lioh Moeller <lioh.moeller@library.ethz.ch>
2023-02-26 22:24:05 +0100

    initial build

--- build/all/app/po/CMakeFiles/gnulinuxch.liohmoeller.pot.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/gnulinuxch.liohmoeller.pot"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/gnulinuxch.liohmoeller.pot.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
